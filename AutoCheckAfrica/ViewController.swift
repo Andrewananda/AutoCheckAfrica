dd9bca05ba2edc1974fcbaf49bda6eca8cdc657d
andrewtiema@gmail.com <andrewtiema@gmail.com>
2022-07-24 11:36:28 +0300

    Initial Commit

---
+++ AutoCheckAfrica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoCheckAfrica
+//
+//  Created by Andrew Ananda on 24/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
